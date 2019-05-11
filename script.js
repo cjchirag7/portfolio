2cb5294590b9959623f6c40d449e8cc88154ab61
cjchirag7 <cjchirag7@gmail.com>
2019-05-11 16:25:28 +0530

    Created navbar, added background image and added top button

---
+++ script.js
@@ -0,0 +1,30 @@
+function topFunction() {
+    document.documentElement.scrollTop = 0;
+  }
+  window.onscroll = function(){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("arrow").style.display = "block";
+    } else {
+      document.getElementById("arrow").style.display = "none";
+    }
+    var nav=document.getElementByClass("navbar");
+    if(document.body.scrollTop() < window.innerHeight-50) {
+    nav.classList.add('trans');
+    } else {
+      nav.classList.remove('trans');
+     }
+
+
+  };
+  $(document).ready(function(){
+
+    $('[data-toggle="tooltip"]').tooltip();
+$(window).scroll(function(){
+if($(this).scrollTop() < window.innerHeight-50) { 
+      $('.navbar').addClass('trans');
+  } else {
+      $('.navbar').removeClass('trans');
+  }
+
+});
+});
