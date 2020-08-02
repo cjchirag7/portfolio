80259c8a8cf8453bf24ee5b67fb776a42447f88e
Chirag Jain <cjchirag7@gmail.com>
2020-08-02 14:38:38 +0530

    Updates

---
+++ precache-manifest.4ec49824f038d12523a7d5b8b134ccab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "da0c4f66a659a826a594",
+    "url": "/static/js/main.da0c4f66.chunk.js"
+  },
+  {
+    "revision": "eff2c0450ebdd9b68da4",
+    "url": "/static/js/1.eff2c045.chunk.js"
+  },
+  {
+    "revision": "da0c4f66a659a826a594",
+    "url": "/static/css/main.d6b8d70b.chunk.css"
+  },
+  {
+    "revision": "03dfa946d010705fa3b7ede087feadba",
+    "url": "/index.html"
+  }
+];
